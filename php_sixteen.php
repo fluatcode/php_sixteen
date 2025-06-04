4bb96cc178c6e33ff8790cfdd51e837d326a490c
fluatcode <fluatcode@gmail.com>
2025-06-04 20:11:21 +0300

    Update php_sixteen.php

---
+++ php_sixteen.php
@@ -0,0 +1,10 @@
+$paper = array(
+  'copier' => "Copier & Multipurpose",
+  'inkjet' => "Inkjet Printer",
+  'laser' => "Laser Printer",
+  'photo' => "Photographic Paper"
+);
+
+while (list($item, $description) = each($paper)) {
+    echo "$item: $description<br>";
+}
